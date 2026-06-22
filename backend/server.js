335799d83e556bc27b64d6eeddc1c122bf6a822c
Asawir <muhammadasawirkhan@gmail.com>
2026-06-22 07:18:55 -0700

    dockerfile

--- backend/server.js
+++ backend/server.js
@@ -41,4 +41,7 @@ app.use("/api/messages", messageRoutes);
 initSocket(io);
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
